a9f6dc4f8285ee552fbd7408d16dcc1eae70a5ef
Nithin412 <kandulanithin03@gmail.com>
2022-02-27 20:23:12 +0530

    creating virtual env using tox

---
+++ tests/conftest.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 27 20:22:00 2022
+
+@author: dell
+"""
---
+++ tests/test_config.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 27 20:22:12 2022
+
+@author: dell
+"""
